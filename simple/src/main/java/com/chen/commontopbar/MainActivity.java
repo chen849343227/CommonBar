c2508c5983c55fa0100a5c7b9f4fb053373e2a98
long <849343227@qq.com>
2017-07-25 15:01:03 +0800

    删除多余的东西

--- simple/src/main/java/com/chen/commontopbar/MainActivity.java
+++ simple/src/main/java/com/chen/commontopbar/MainActivity.java
@@ -12,8 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CommonBar commonBar  = (CommonBar) findViewById(R.id.commonBar);
-
-
     }
 }
